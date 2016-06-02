641df2871aaf26a85bddca066e7e86a7d78161e1
Renato Crivano <renato.crivano@gmail.com>
2016-06-02 18:49:55 -0300

    Initial commit

---
+++ src/main/java/br/jus/trf2/textoweb/signer/TextoWebSignerServlet.java
@@ -0,0 +1,24 @@
+package br.jus.trf2.textoweb.signer;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+
+import com.crivano.swaggerservlet.Swagger;
+import com.crivano.swaggerservlet.SwaggerServlet;
+
+public class TextoWebSignerServlet extends SwaggerServlet {
+	private static final long serialVersionUID = -1611417120964698257L;
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+
+		super.setActionPackage("br.jus.trf2.textoweb.signer");
+
+		Swagger sw = new Swagger();
+		sw.loadFromInputStream(this.getServletContext().getResourceAsStream(
+				"/api/v1/swagger.yaml"));
+
+		super.setSwagger(sw);
+	}
+}
---
+++ src/main/resources/br/jus/trf2/textoweb/signer/list.sql
@@ -0,0 +1,21 @@
+select	tw.CodSecao as IdSecao, tw.CodTxtWeb as IdTextoWeb, tw.Arq as ArquivoCompactado, ttw.Descr as TipoDeTexto, p.NumProcCompl as CodDocumento, ti.Descr as IncidenteDocumento
+from	TextoWeb tw
+inner 	join SituacaoTextoWeb stw on stw.CodSitua = nval_const('$$SituaTxtWebRevis') and stw.CodSecao = tw.CodSecao and stw.CodSitua = tw.Codsitua
+inner	join TipoTextoWeb ttw on ttw.CodSecao = tw.CodSecao and ttw.CodTipTxt = tw.CodTipTxt
+inner 	join Usuario u on u.CodSecao = tw.CodSecao and u.CodUsu = tw.CodUsuResp
+inner	join t_Processo p on p.CodSecao = tw.CodSecao and p.CodDoc = tw.CodDoc
+left	outer join ProcessoIncidente pi on pi.CodSecao = tw.CodSecao and pi.CodDoc = tw.CodDoc and pi.SeqIncid = tw.SeqIncid
+left	outer join TipoIncidente ti on ti.CodSecao = pi.CodSecao and ti.CodTipIncid = pi.CodTipIncid
+where	tw.Arq is not null
+and	tw.ArqAssin is null
+and	tw.CodUsuResp in
+	(
+		select	distinct m.codusu
+		from 	magistrado m, pessoadocumento pd
+		where	pd.CodtipDocPess = 1
+		and	pd.IndAtivo = 'S'
+		and	m.codusu = tw.CodUsuResp
+		and	m.codsecao = pd.codsecao
+		and	m.codpess = pd.codpess
+		and	pd.NumDocPess = ?
+	)
