8fb9b3e8d65f9f6387917d9168151f707769a468
Renato Crivano <renato.crivano@gmail.com>
2017-04-07 12:50:12 -0300

    Atualizando o usuário responsável

--- src/main/resources/br/jus/trf2/textoweb/signer/save.sql
+++ src/main/resources/br/jus/trf2/textoweb/signer/save.sql
@@ -1,30 +1,44 @@
-declare 
-	-- parametros de entrada
-	i_codsecao number(2,0);
-	i_codtxtweb number(10,0);
-	i_envelopecomprimido blob;
-  	i_cpf varchar2(14);
-  	
-  	-- parametros de saida
-  	o_status varchar2(32767) := NULL;
-  	o_error varchar2(32767) := NULL;
-  	
-  	v_login varchar2(200) := NULL;
-begin
-	i_codsecao := ?;
-	i_codtxtweb := ?;
-	i_envelopecomprimido := ?;
-	i_cpf := ?;
+DECLARE 
+    -- parametros de entrada 
+    i_codsecao           NUMBER(2, 0); 
+    i_codtxtweb          NUMBER(10, 0); 
+    i_envelopecomprimido BLOB; 
+    i_cpf                VARCHAR2(14); 
+    -- parametros de saida 
+    o_status             VARCHAR2(32767) := NULL; 
+    o_error              VARCHAR2(32767) := NULL; 
+    v_login              VARCHAR2(200) := NULL; 
+    v_codusu             NUMBER(6, 0) := NULL; 
+BEGIN 
+    i_codsecao := ?; 
 
-  	-- identifica o usuario perante o sistema de auditoria
-  	select login into v_login from usuario where numcpf = i_cpf and IndAtivo = 'S';
-  	dbms_session_set_context(v_login);
-  	
-	-- grava a assinatura e atualiza a situacao.
-	update TextoWeb set ArqAssin = i_envelopecomprimido, CodSitua = nval_const('$$SituaTxtWebAssin') where	CodSecao = i_codsecao and CodTxtWeb = i_codtxtweb;
-			
-	o_status := 'OK';
+    i_codtxtweb := ?; 
 
-	? := o_status;
-	? := o_error;
-end;
+    i_envelopecomprimido := ?; 
+
+    i_cpf := ?; 
+
+    -- identifica o usuario perante o sistema de auditoria 
+    SELECT login, 
+           codusu 
+    INTO   v_login, v_codusu 
+    FROM   usuario 
+    WHERE  numcpf = i_cpf 
+           AND indativo = 'S'; 
+
+    Dbms_session_set_context(v_login); 
+
+    -- grava a assinatura e atualiza a situacao. 
+    UPDATE textoweb 
+    SET    arqassin = i_envelopecomprimido, 
+           codsitua = Nval_const('$$SituaTxtWebAssin'), 
+           codusuresp = v_codusu 
+    WHERE  codsecao = i_codsecao 
+           AND codtxtweb = i_codtxtweb; 
+
+    o_status := 'OK'; 
+
+    ? := o_status; 
+
+    ? := o_error; 
+END; 
